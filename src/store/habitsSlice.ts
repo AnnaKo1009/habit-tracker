5b591566e02aa79975cfa79e0cf9f3c80dab8fa3
AnnaKo1009 <anna.conovalova2018@gmail.com>
2026-06-15 12:43:45 +0300

    feat:  в слайсы добавлена логика

--- src/store/habitsSlice.ts
+++ src/store/habitsSlice.ts
@@ -1,9 +1,48 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice,  type PayloadAction } from '@reduxjs/toolkit';
+import { mockHabits } from '../mocks/habits';
+
+interface Habit {
+    id: number;
+    name: string; 
+    completed: boolean;
+    completedDates: string[];
+}
+
+interface HabitsState {
+    items: Habit[];
+}
+
+const initialState: HabitsState = {
+    items: mockHabits,
+}
 
 const habitsSlice = createSlice({
     name: 'habits',
-    initialState: { items: [] },
-    reducers: {},
+    initialState,
+    reducers: {
+        addHabit: (state, action: PayloadAction<Habit>) => {
+            state.items.unshift(action.payload);
+        },
+        deleteHabit: (state, action: PayloadAction<number>) => {
+            state.items = state.items.filter(h => h.id !== action.payload);
+        },
+        editHabit: (state, action: PayloadAction<{id: number; name: string}>) => {
+            const habit = state.items.find(h => h.id === action.payload.id);
+            if (habit) habit.name = action.payload.name;
+        },
+        toggleHabitDate: (state, action: PayloadAction<{id:number, date: string}>) => {
+            const habit = state.items.find (h => h.id === action.payload.id);
+            if (habit) {
+                const index = habit.completedDates.indexOf(action.payload.date)
+                if (index === -1) {
+                    habit.completedDates.push(action.payload.date)
+                } else {
+                    habit.completedDates.splice(index, 1);
+                }
+            }
+        }
+    },
 });
 
+export const {addHabit, deleteHabit, editHabit, toggleHabitDate} = habitsSlice.actions; 
 export default habitsSlice.reducer;
--- src/store/userSlice.ts
+++ src/store/userSlice.ts
@@ -1,9 +1,25 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+interface UserState {
+    name: string;
+    avatar: string;
+}
+
+const initialState: UserState = {
+    name: 'Anna',
+    avatar: 'https://doctor-veterinar.ru/images/mini-pig.png'
+}
+
 const userSlice = createSlice({
     name: 'user',
-    initialState: { name: '', avatar: '' },
-    reducers: {},
+    initialState,
+    reducers: {
+        updateUser: (state, action ) => {
+            state.name = action.payload.name;
+            state.avatar = action.payload.avatar;
+        },
+    },
 });
 
+export const { updateUser } = userSlice.actions;
 export default userSlice.reducer;
